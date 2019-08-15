c9223b528c538af62b97691e83d8a504659c8f47
andre <andrer54@gmail.com>
2019-08-15 19:41:02 -0300

    Adicionar arquivos de projeto.

---
+++ AgendaTelefonica/Models/contexto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace AgendaTelefonica.Models
+{
+    public class contexto: DbContext
+    {
+        public contexto() : base("name=principal")
+        {
+        }
+        public DbSet<pessoa> Pessoas { get; set; }
+    }
+}
